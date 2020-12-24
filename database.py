99e733e9aacd708d4db438c2d46a5eb94646a53a
otal18 <hakan.otal@outlook.com>
2020-12-24 15:26:09 +0300

    fixed some errors

--- database.py
+++ database.py
@@ -23,23 +23,24 @@ class Database:
         rows = cur.fetchall()
         cur.close()
         for r in rows:
-            pilot = self.get_pilot_name(r[2])
-            plane = self.get_plane_name(r[5])
-            cap = self.get_plane_cap(r[5])
-            flights.append(Flight(r[0], r[1], pilot, r[3], r[4], plane, cap, r[6]))
+            pilot = self.get_pilot(r[2])
+            plane = self.get_plane(r[5])
+            flights.append(Flight(r[0], r[1], pilot[1], r[3], r[4], plane[1], plane[3], r[6]))
         return flights
 
     def get_flight(self, id):
-        for flight in self.get_flights:
-            if flight.id == id:
-                return flight
+        cur = self.conn.cursor()
+        cur.execute("SELECT * FROM flights WHERE flight_id = %s", (id,))
+        r = cur.fetchone()
+        cur.close()
+        return r
 
-    def get_pilot_name(self, id):
+    def get_pilot(self, id):
         cur = self.conn.cursor()
         cur.execute("SELECT * FROM pilots WHERE pilot_id = %s", (id,))
         r = cur.fetchone()
         cur.close()
-        return r[1]
+        return r
 
     def get_pilots(self):
         pilots = []
@@ -51,19 +52,12 @@ class Database:
             pilots.append(Pilot(r[0], r[1], r[2]))
         return pilots
 
-    def get_plane_cap(self, id):
+    def get_plane(self, id):
         cur = self.conn.cursor()
         cur.execute("SELECT * FROM planes WHERE plane_id = %s", (id,))
         r = cur.fetchone()
         cur.close()
-        return r[3]
-
-    def get_plane_name(self, id):
-        cur = self.conn.cursor()
-        cur.execute("SELECT * FROM planes WHERE plane_id = %s", (id,))
-        r = cur.fetchone()
-        cur.close()
-        return r[1]
+        return r
 
     def get_planes(self):
         planes = []
